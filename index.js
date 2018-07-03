d5f67895868fb5e386a96ebc05746f9169448a43
Viktor Ivanov <sessgreem@gmail.com>
2018-07-03 19:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ function updateObjectWithKeyAndValue(object,key,value){
   
   return Object.assign({},object,{key:value});
 }
-//function 
+function  destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key] = value;
+  return object;
+  
+}
