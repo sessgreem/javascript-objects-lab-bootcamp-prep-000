6a1806d0b6c1f6b077a77e70fc3cd2fe99dc46b6
Viktor Ivanov <sessgreem@gmail.com>
2018-07-03 19:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ function  destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object;
 }
 function deleteFromObjectByKey(object,key){
-  
-  return Object.assign({},object,delete object[key]);
+ return Object.assign({},object,delete object[key]);
 }
